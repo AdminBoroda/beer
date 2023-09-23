43088be04c108a49a82a5e741391fffadc6133d2
Dmitriy <9266004760@bk.ru>
2023-09-23 13:21:42 +0300

    ffdf

---
+++ beer.py
@@ -0,0 +1,5 @@
+beer1 = input('Введи, сколько у тебя литров пива: ')
+beer2 = input('Сколько пива принес друг: ')
+beer3 = int(beer1) + int(beer2)
+answer = 'На двоих у вас: ' + str(beer3) + ' литров пива'
+print(answer)
